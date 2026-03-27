582cd8a1f4f53af10852f09044983923a3aa1a4f
Alexander Schumann <aschuma4@student10.cse.nd.edu>
2026-03-26 21:40:05 -0400

    Last vuln found in Part4

--- HW3/Part4.c
+++ HW3/Part4.c
@@ -4,17 +4,21 @@ int main()
 {
   int i, j, la, lb, num = 0;
   char c, a[12], b[45];
+  // Vulnerability 1: No bounds checking on input, allowing for buffer overflow
   while ((c = getchar()) != '\n')
     a[num++] = c;
+  // Vulnerability 2: Same as 1 really, but could be writing out of bounds of a if num > 11
   a[num] = '\0';
   printf("The str1 is:\n");
-  printf(a);
+  printf("%s\n", a);
   num = 0;
+  // Vulnerability 3: No bounds checking on input, allowing for buffer overflow
   while ((c = getchar()) != '\n')
     b[num++] = c;
+  // Vulnerability 4: Same as 3 really, but could be writing out of bounds of b if num > 44
   b[num] = '\0';
   printf("\nThe str2 is:\n");
-  printf(b);
+  printf("%s\n", b);
   la = strlen(a);
   lb = strlen(b);
   for (i = 0; (lb - i) >= la; i++)
@@ -22,6 +26,7 @@ int main()
     if (b[i] == a[0])
     {
       for (j = 0; j < la; j++)
+        // Vulnerability 5: No bounds checking to see if i + j is out of bounds of b
         if (b[i + j] != a[j])
           break;
       if (j == la)
